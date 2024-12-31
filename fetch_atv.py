7024371044ae0d44de242ab484a14814a3215990
worldmediaman <52355096+worldmediaman@users.noreply.github.com>
2024-12-31 10:25:02 +0100

    Update fetch_atv.py

--- fetch_atv.py
+++ fetch_atv.py
@@ -1,4 +1,15 @@
-import logging import http.client as http_client http_client.HTTPConnection.debuglevel = 1 logging.basicConfig() logging.getLogger().setLevel(logging.DEBUG) requests_log = logging.getLogger("requests.packages.urllib3") requests_log.setLevel(logging.DEBUG)
+import logging
+import http.client as http_client
+
+http_client.HTTPConnection.debuglevel = 1
+
+logging.basicConfig()
+logging.getLogger().setLevel(logging.DEBUG)
+requests_log = logging.getLogger("requests.packages.urllib3")
+requests_log.setLevel(logging.DEBUG)
+requests_log.propagate = True
+
+# Dein ursprünglicher Code hier
 
 import requests
 import json

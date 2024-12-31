3cb4579bd0f2e2631d858d1fecd7018531b6293f
worldmediaman <52355096+worldmediaman@users.noreply.github.com>
2024-12-31 10:08:47 +0100

    Update fetch_atv.py

--- fetch_atv.py
+++ fetch_atv.py
@@ -13,7 +13,10 @@ output_file_path = "result/List/ATV.m3u8"
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
-    'Accept-Language': 'en-US,en;q=0.9'
+    'Accept-Language': 'en-US,en;q=0.9',
+    'Referer': 'https://www.atvavrupa.tv/canli-yayin',
+    'Origin': 'https://www.atvavrupa.tv',
+    'Connection': 'keep-alive'
 }
 
 def fetch_and_save_atv():
